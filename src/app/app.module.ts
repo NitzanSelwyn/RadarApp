e5aff73d5daddf77dc1f3e5e2dfc379a0eb7c363
nitzan selwyn <nitzansel@gmail.com>
2018-06-24 10:15:39 +0300

    Ganariting login & registar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegistarComponent } from './user/registar/registar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistarComponent
   ],
   imports: [
     BrowserModule
