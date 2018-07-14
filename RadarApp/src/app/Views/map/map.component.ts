473bc789a7296c6645f47f0604bf5d4c4bfa83ab
nitzan selwyn <nitzansel@gmail.com>
2018-07-14 21:50:58 +0300

    Reverse Geocoding to get the address name from LatLng

--- RadarApp/src/app/Views/map/map.component.ts
+++ RadarApp/src/app/Views/map/map.component.ts
@@ -20,7 +20,10 @@ export class MapComponent implements OnInit {
   constructor(private mapsAPI: MapsAPILoader, private ngZone: NgZone, private apiService: ApiService) {
     this.destLat = 55.930;
     this.destLng = -3.118;
+
+
     this.getUserLocation();
+
 
 
   }
@@ -32,49 +35,56 @@ export class MapComponent implements OnInit {
   ngOnInit() {
 
     this.apiService.getEvents();
+    // this.calculate();
 
-    var origin = new google.maps.LatLng(this.lat, this.lng);//your corrent position
 
-    var destination = new google.maps.LatLng(this.destLat, this.destLng);//positions from DB
 
 
     //calculate Distance using DistanceMatrix
-    var service = new google.maps.DistanceMatrixService();
-    service.getDistanceMatrix(
-      {
-        origins: [origin],
-        destinations: [destination],
-        travelMode: google.maps.TravelMode.WALKING,
-        unitSystem: google.maps.UnitSystem.METRIC,
-      }, callback);
+    // var service = new google.maps.DistanceMatrixService();
+    // service.getDistanceMatrix(
+    //   {
+    //     origins: [origin],
+    //     destinations: [destination],
+    //     travelMode: google.maps.TravelMode.WALKING,
+    //     unitSystem: google.maps.UnitSystem.METRIC,
+    //   }, callback);
 
-    console.log('befor search')
-    function callback(response, status) {
-      if (status == 'OK') {
-        var origins = response.originAddresses;
-        var destinations = response.destinationAddresses;
+    // console.log('befor search')
+    // function callback(response, status) {
+    //   if (status == 'OK') {
+    //     var origins = response.originAddresses;
+    //     var destinations = response.destinationAddresses;
 
 
-        for (var i = 0; i < origins.length; i++) {
-          var results = response.rows[i].elements;
-          for (var j = 0; j < results.length; j++) {
-            var element = results[j];
-            var distance = element.distance;
-            var duration = element.duration;
-            var from = origins[i];
-            var to = destinations[j];
-            console.log(from + "To: " + to)
-            console.log("distance is: " + distance + "  duration is: " + duration)
-          }
-        }
-      }
-    }
+    //     for (var i = 0; i < origins.length; i++) {
+    //       var results = response.rows[i].elements;
+    //       for (var j = 0; j < results.length; j++) {
+    //         var element = results[j];
+    //         var distance = element.distance;
+    //         var duration = element.duration;
+    //         var from = origins[i];
+    //         var to = destinations[j];
+    //         console.log(from + "To: " + to)
+    //         console.log("distance is: " + distance + "  duration is: " + duration)
+    //       }
+    //     }
+    //   }
+    // }
 
 
   }
 
 
 
+  // calculate() {
+  //   var origin = new google.maps.LatLng(this.lat, this.lng);//your corrent position
+  //   console.log(origin);
+  //   var destination = new google.maps.LatLng(this.destLat, this.destLng);//positions from DB
+  //   console.log(destination);
+  //   const distance = google.maps.geometry.spherical.computeDistanceBetween(origin, destination);
+  //   console.log(distance);
+  // }
 
 
 
@@ -90,9 +100,14 @@ export class MapComponent implements OnInit {
 
 
 
+  city: any;
 
 
 
+
+  setTheAddress(str: String) {
+    this.city = str;
+  }
 
   getUserLocation() {
     /// locate the user
@@ -100,6 +115,20 @@ export class MapComponent implements OnInit {
       navigator.geolocation.getCurrentPosition(position => {
         this.lat = position.coords.latitude;
         this.lng = position.coords.longitude;
+        let geocoder = new google.maps.Geocoder();
+        let latlng = new google.maps.LatLng(this.lat, this.lng);
+        let request = {
+          location: latlng
+        };
+        geocoder.geocode(request, (results, status) => {
+          if (status == google.maps.GeocoderStatus.OK) {
+            if (results[0] != null) {
+              console.log(results[0].address_components[results[0].address_components.length - 4].short_name);
+              this.setTheAddress(results[0].address_components[results[0].address_components.length - 4].short_name);
+            }
+          }
+        });
+
       });
 
     }
